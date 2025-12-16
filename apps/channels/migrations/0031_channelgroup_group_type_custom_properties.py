23ba275bf23e59099e30be1077ba9fbbd64e1008
sbeimel <60073742+sbeimel@users.noreply.github.com>
2025-12-16 19:48:02 +0100

    Update 0031_channelgroup_group_type_custom_properties.py

--- apps/channels/migrations/0031_channelgroup_group_type_custom_properties.py
+++ apps/channels/migrations/0031_channelgroup_group_type_custom_properties.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('channels', '0030_alter_stream_url'),
+        ('dispatcharr_channels', '0030_alter_stream_url'),
     ]
 
     operations = [
