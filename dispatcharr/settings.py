b38451ed530f63cf207738275122587ca01991cd
sbeimel <sbeimel@web.de>
2026-02-02 11:59:47 +0100

    Update settings.py

--- dispatcharr/settings.py
+++ dispatcharr/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     "apps.vod.apps.VODConfig",
     "core",
     "daphne",
-    "drf_yasg",
+    # "drf_yasg",  # Temporarily disabled - not installed
     "channels",
     "django.contrib.admin",
     "django.contrib.auth",
