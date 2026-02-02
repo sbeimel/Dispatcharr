f3825b451a28e1e05f270ea6df8a0c5e91f22e71
sbeimel <sbeimel@web.de>
2026-02-02 11:27:19 +0100

    .

--- apps/proxy/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-app_name = 'proxy'
-
-urlpatterns = [
-    path('ts/', include('apps.proxy.ts_proxy.urls')),
-    path('hls/', include('apps.proxy.hls_proxy.urls')),
-    path('vod/', include('apps.proxy.vod_proxy.urls')),
-]
--- dispatcharr/wsgi.py
+++
@@ -1,8 +0,0 @@
-"""
-WSGI config for dispatcharr project.
-"""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dispatcharr.settings')
-application = get_wsgi_application()
